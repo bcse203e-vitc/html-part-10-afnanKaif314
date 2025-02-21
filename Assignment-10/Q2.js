13b7dfb14f1409ca1ce38da99d00d5aa240f3cc3
afnanKaif314 <afnankaif056@gmail.com>
2025-02-21 12:39:23 +0530

    Create Q2.js

---
+++ Assignment-10/Q2.js
@@ -0,0 +1,7 @@
+document.getElementById('redBtn').addEventListener('click', function() {
+    document.body.style.backgroundColor = 'red';
+});
+
+document.getElementById('blueBtn').addEventListener('click', function() {
+    document.body.style.backgroundColor = 'blue';
+});
